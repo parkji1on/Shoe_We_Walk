3c06f56dd690a968ea773f2c0bae23ed991c83d8
parkjiwon <ji11won@naver.com>
2023-05-24 18:41:01 +0900

    CHORE : change enviroment for NaverMap, Pedometer
    build.gradle, gradle.properties, settings.gradle NaverMap을 위한 설정 변경
    Manifest.xml : 위치추적과 기기내 센서를 위한 permission 추가

--- app/src/main/java/com/example/shoe_we_walk/WalkActivity.kt
+++ app/src/main/java/com/example/shoe_we_walk/WalkActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shoe_we_walk.Util.setStatusBarTransparent
 import com.example.shoe_we_walk.databinding.ActivityWalkBinding
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class WalkActivity : AppCompatActivity() {
+class WalkActivity : AppCompatActivity(), OnMapReadyCallback{
 
     private lateinit var binding: ActivityWalkBinding
 
@@ -16,8 +19,49 @@ class WalkActivity : AppCompatActivity() {
 
         this.setStatusBarTransparent()  //statusBar transparent
 
+        binding.mapMv.onCreate(savedInstanceState)
+
         binding.exerciseFinishBtn.setOnClickListener {//운동종료
 //            TODO("특정 정보를 가지고 HomeFragment로 이동")
         }
+    }
+
+    override fun onMapReady(p0: NaverMap) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapMv.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapMv.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapMv.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapMv.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapMv.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapMv.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapMv.onLowMemory()
     }
 }
